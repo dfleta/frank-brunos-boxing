e543768708dd2ab13326a16f7ae5318e2bf9256c
David Gelpi <gelpiorama@gmail.com>
2024-03-07 14:05:00 +0100

    feat: Crear las tres tarjetas, una por juez

--- src/main/java/edu/badpals/Brunosbox.java
+++ src/main/java/edu/badpals/Brunosbox.java
@@ -1,13 +1,56 @@
 package edu.badpals;
 
-/**
- * Hello world!
- *
- */
+import com.github.freva.asciitable.AsciiTable;
+
 public class Brunosbox 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Bruno!" );
+    public static void main( String[] args ) {
+
+        System.out.println("\n\" - I see three of them out there. \n - Hit the one in the middle.- Rocky Balboa\n");
+
+        String[] headers = {"Round 1", "Round 2", "Round 3", "Round 4", "Round 5", 
+                            "Round 6", "Round 7", "Round 8", "Round 9", "Round 10"};
+
+        String[][] data = {
+
+            // ScoreCard White
+            {"9 - 10", 
+             "9 - 10", 
+             "9 - 10", 
+             "8 - 10", // knockdown
+             "10 - 8", // knockdown
+             "10 - 8", // referee point deduction
+             "10 - 9", 
+             "9 - 10", 
+             "10 - 9", 
+             "10 - 9"},
+            
+             // ScoreCard Blue
+            {"9 - 10", 
+             "9 - 10", 
+             "9 - 10", 
+             "8 - 10", // knockdown
+             "10 - 8", // knockdown
+             "10 - 8", // referee point deduction
+             "10 - 9", 
+             "9 - 10", 
+             "10 - 9", 
+             "10 - 9"},
+
+            // ScoreCard Pink
+            {"9 - 10", 
+             "9 - 10", 
+             "9 - 10", 
+             "8 - 10", // knockdown
+             "10 - 8", // knockdown
+             "10 - 8", // referee point deduction
+             "10 - 9", 
+             "9 - 10", 
+             "10 - 9", 
+             "10 - 9"}
+
+        };
+
+        System.out.println(AsciiTable.getTable(headers, data));
     }
 }

20c04fc0aaa842c810406c836c99191c9f1d4088
David Gelpi <gelpiorama@gmail.com>
2024-03-07 13:44:33 +0100

    build: :rocket: Configurar dependencias del proyecto
    Añadir documentación basica

---
+++ src/main/java/edu/badpals/Brunosbox.java
@@ -0,0 +1,13 @@
+package edu.badpals;
+
+/**
+ * Hello world!
+ *
+ */
+public class Brunosbox 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Bruno!" );
+    }
+}

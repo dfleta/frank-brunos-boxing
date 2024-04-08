bc68848cd9b83dc0b0b64a577f3e6d95491959fc
David Gelpi <gelpiorama@gmail.com>
2024-04-08 18:02:01 +0200

    refactor(ScoreCard): Convertir en Optional el tipo de Round
    Encapsular acceso a las estructuras en ScoreCard

--- src/test/java/edu/badpals/brunosbox/ScoreCardTest.java
+++ src/test/java/edu/badpals/brunosbox/ScoreCardTest.java
@@ -10,7 +10,7 @@ public class ScoreCardTest {
 
     private ScoreCard card;
 
-    private String[] judgeScoreCard =  {"9 - 10", 
+    private String[] whiteScoreCard =  {"9 - 10", 
                                         "9 - 10", 
                                         "9 - 10", 
                                         "9 - 10",
@@ -21,25 +21,61 @@ public class ScoreCardTest {
                                         "10 - 9", 
                                         "9 - 10"};
 
+    private String[] pinkScoreCard = {"9 - 10", 
+                                        "9 - 10", 
+                                        "9 - 10", 
+                                        "1, 8 - 10", // referee point deduction
+                                        "10 - 8",    // knockdown
+                                        "10 - 8 ,1", // referee point deduction
+                                        "10 - 9", 
+                                        "9 - 10", 
+                                        "10 - 9", 
+                                        "10 - 8"}; // knockdown
+
     @BeforeEach
     public void setup() {
         card = new ScoreCard("white");
     }
     
     @Test
-    public void loadJudgeScoreCardTest() {
+    public void loadJudgeScoreCardRoundFactoryRegularTest() {
 
-        card.loadJudgeScoreCard(judgeScoreCard);
+        card.loadJudgeScoreCard(whiteScoreCard);
         assertEquals(10, card.getNumRounds());
         assertTrue(card.getRounds().stream().allMatch(a -> a instanceof Round));
         assertTrue(card.getRounds().stream().allMatch(a -> a instanceof RegularRound));
     } 
 
     @Test
+    public void loadJudgeScoreCardRoundFactoryNullTest() {
+        card.loadJudgeScoreCard(new String[]{null, null});
+        assertEquals(0, card.getNumRounds());
+    }
+
+    @Test
+    public void loadJudgeScoreCardRoundFactoryPointsDeductedTest() {
+
+        card.loadJudgeScoreCard(pinkScoreCard);
+        assertEquals(10, card.getNumRounds());
+        assertTrue(card.getRounds().stream().allMatch(a -> a instanceof Round));
+        assertEquals(2, card.getRounds().stream().filter(a -> a instanceof PointsDeducted).count());
+    }
+
+
+    @Test
+    public void loadJudgeScoreCardRoundFactoryKnockDownTest() {
+
+        card.loadJudgeScoreCard(pinkScoreCard);
+        assertEquals(10, card.getNumRounds());
+        assertTrue(card.getRounds().stream().allMatch(a -> a instanceof Round));
+        assertEquals(2, card.getRounds().stream().filter(a -> a instanceof KnockdownRound).count());
+    }
+
+    @Test
     public void getBoxerFinalScoreTest() {
         assertEquals(0, card.getRedBoxerFinalScore());
         assertEquals(0, card.getBlueBoxerFinalScore());
-        card.loadJudgeScoreCard(judgeScoreCard);
+        card.loadJudgeScoreCard(whiteScoreCard);
         assertEquals(94, card.getRedBoxerFinalScore());
         assertEquals(96, card.getBlueBoxerFinalScore());
     }

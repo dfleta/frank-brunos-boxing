47056c5a34c7170f18dca2ed156ec083e24a153c
David Gelpi <gelpiorama@gmail.com>
2025-03-11 14:16:28 +0100

    refactor(RoundFactory): Abstraer separadores puntos deducidos y round

--- src/main/java/edu/badpals/brunosbox/RoundFactory.java
+++ src/main/java/edu/badpals/brunosbox/RoundFactory.java
@@ -2,18 +2,21 @@ package edu.badpals.brunosbox;
 
 public class RoundFactory {
 
+    private static final char POINTS_DEDUCTED_MARK = ',';
+    private static final char ROUND_POINTS_MARK = '-';
+
     public static Round getRound(String roundScore){
     
         if (roundScore == null) { 
             return null;
         }
-        if (!roundScore.contains(",")) {
+        if (roundScore.indexOf(POINTS_DEDUCTED_MARK) == -1) {
             return new RegularRound(roundScore);
         } 
-        else if (roundScore.equalsIgnoreCase("1, 8 - 10")) {
+        else if (roundScore.indexOf(POINTS_DEDUCTED_MARK) < roundScore.indexOf(ROUND_POINTS_MARK)) {
             return new PointsDeductedRed(roundScore);
         }
-        else if (roundScore.equalsIgnoreCase("10 - 8 ,1")) {
+        else if (roundScore.indexOf(POINTS_DEDUCTED_MARK) > roundScore.indexOf(ROUND_POINTS_MARK)) {
             return new PointsDeductedBlue(roundScore);
         }
         return null;
